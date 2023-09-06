057cc101a28dfbab79f5e775b3acb1aab7cb611c
AlexDjangoX <alexmonk17@gmail.com>
2023-09-06 20:13:08 +0200

    BUG: loader working when CRUD

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '@clerk/nextjs';
-import { useToast } from '@/components/ui/use-toast';
 
 import ProfileHeading from '@/components/ProfileHeading';
 import RentedCars from '@/components/profilePageComponents/RentedCars';
@@ -14,9 +13,12 @@ import {
   fetchAddedCars,
   fetchUserReviews,
 } from '@/components/profilePageComponents/profile.utils';
+import { useToast } from '@/components/ui/use-toast';
+import Loader from '@/components/transitionPages/Loader';
 
 const ProfilePage: React.FC = () => {
   const [userData, setUserData] = useState<UserParams | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [carsRented, setCarsRented] = useState<string | null>(null);
   const [addedCars, setAddedCars] = useState<string | null>(null);
   const [reviews, setReviews] = useState<string | null>(null);
@@ -24,6 +26,7 @@ const ProfilePage: React.FC = () => {
   const { toast } = useToast();
 
   useEffect(() => {
+    setIsLoading(true);
     async function fetchData() {
       if (isLoaded && userId) {
         const mongoUserId = await fetchUserData(userId, setUserData, toast);
@@ -33,23 +36,29 @@ const ProfilePage: React.FC = () => {
           await fetchUserReviews(mongoUserId, setReviews, toast);
         }
       }
+      setIsLoading(false);
     }
 
     fetchData();
   }, [userId, isLoaded, toast]);
 
-  return (
-    <div className="flex w-full justify-center self-center bg-white200 dark:bg-gray900">
-      <div className="mt-20 flex w-full max-w-[90rem] flex-col p-6 md:mt-40">
-        <ProfileHeading
-          userData={JSON.stringify(userData)}
-          reviews={reviews || '{}'}
-        />
-        <RentedCars rentedCars={carsRented || ''} />
-        <UsersCarsForRent carsForRent={addedCars || ''} />
+  if (isLoading) {
+    return <Loader />;
+  } else {
+    return (
+      <div className="flex w-full justify-center self-center bg-white200 dark:bg-gray900">
+        <div className="mt-20 flex w-full max-w-[90rem] flex-col p-6 md:mt-40">
+          <ProfileHeading
+            userData={JSON.stringify(userData)}
+            reviews={reviews || '{}'}
+          />
+
+          <RentedCars rentedCars={carsRented || ''} />
+          <UsersCarsForRent carsForRent={addedCars || ''} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ProfilePage;
