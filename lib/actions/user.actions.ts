d4af0d2a4e883d0004d03cbb0c44fd4dc5794ed5
DevTaehong <minth1123@icloud.com>
2024-02-05 02:13:06 -0400

    refactor: refactor user.actions.ts file

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,29 +1,30 @@
-'use server';
+"use server";
 
-import { revalidatePath } from 'next/cache';
-import { connectToDB } from '../mongoose';
-import User from '../models/user.model';
-import Car from '../models/car.model';
-import Review from '../models/review.model';
-import { UserParams } from '../interfaces';
+import { revalidatePath } from "next/cache";
+
+import { connectToDB } from "../mongoose";
+import User from "../models/user.model";
+import Car from "../models/car.model";
+import Review from "../models/review.model";
+import { CarParamsExtended, UserParams } from "../interfaces";
 
 export async function userFromDB({
-  userName,
+  userClerkId,
   isClientFetch = false,
 }: {
-  userName: string | undefined;
+  userClerkId: string | undefined | null;
   isClientFetch?: boolean;
 }): Promise<UserParams | null> {
   await connectToDB();
 
-  const userDocument = await User.findOne({ id: userName });
+  const userDocument = await User.findOne({ id: userClerkId });
 
   if (userDocument && isClientFetch) {
     return userDocument.toObject();
   }
 
   if (!userDocument) {
-    console.warn('User not found.');
+    console.warn("User not found.");
     return null;
   }
 
@@ -33,39 +34,28 @@ export async function userFromDB({
 export async function addRentedCarToUser(
   userId: string,
   carId: string
-): Promise<UserParams | null> {
-  console.log('Starting addRentedCarToUser function...');
+): Promise<void> {
+  try {
+    await connectToDB();
 
-  await connectToDB();
-  console.log('Connected to the database.');
+    const user = await User.findOne({ id: userId }).exec();
+    if (!user) {
+      console.error("User not found.");
+    }
 
-  // Find the user with the provided id
-  console.log(`Looking for user with id: ${userId}`);
-  const user = await User.findOne({ id: userId }).exec();
-  console.log(user);
-  if (!user) {
-    console.log('User not found.');
-    return null;
+    // Check if the car is already in the user's carsRented array
+    if (user.carsRented.includes(carId)) {
+      console.warn("Car already added to the user's rented cars.");
+      return user.toObject();
+    }
+
+    // Add the car to the user's carsRented array
+    user.carsRented.push({ car: carId });
+
+    await user.save();
+  } catch (error) {
+    console.error(`Failed to add rented car to user: ${error}`);
   }
-  console.log(user + '- User found.');
-
-  // Check if the car is already in the user's carsRented array
-  if (user.carsRented.includes(carId)) {
-    console.warn("Car already added to the user's rented cars.");
-    return user.toObject();
-  }
-  console.log(
-    "Car not yet added to the user's rented cars. Proceeding to add..."
-  );
-
-  // Add the car to the user's carsRented array
-  user.carsRented.push({ car: carId });
-  console.log(`Car with id ${carId} added to the user's carsRented array.`);
-
-  await user.save();
-  console.log('User updated successfully.');
-
-  return user.toObject();
 }
 
 export async function fetchUserCars(
@@ -73,11 +63,11 @@ export async function fetchUserCars(
 ): Promise<UserParams | null> {
   await connectToDB();
   const userWithCars = await User.findOne({ id: userId })
-    .populate('cars')
+    .populate("cars")
     .exec();
 
   if (!userWithCars) {
-    console.warn('User not found.');
+    console.warn("User not found.");
     return null;
   }
 
@@ -85,7 +75,8 @@ export async function fetchUserCars(
 }
 
 export async function updateUser(params: UserParams): Promise<void> {
-  const { id, username, name, bio, image, onboarded, path, email } = params;
+  const { id, username, name, bio, image, onboarded, path, email, coverImage } =
+    params;
   try {
     await connectToDB();
 
@@ -98,9 +89,11 @@ export async function updateUser(params: UserParams): Promise<void> {
         image,
         onboarded,
         email,
+        coverImage,
       },
       { upsert: true }
     );
+
     if (path === `/profile/edit`) {
       revalidatePath(path);
     }
@@ -115,7 +108,7 @@ export async function deleteUserAndCars(id: string): Promise<void> {
 
     const user = await User.findOne({ id });
     if (!user) {
-      throw new Error('User not found.');
+      throw new Error("User not found.");
     }
 
     await Car.deleteMany({ userId: user._id });
@@ -131,11 +124,11 @@ export async function fetchReviewsByUser(id: string): Promise<any[] | null> {
 
   try {
     const userReviews = await Review.find({ id })
-      .populate('carId', 'carTitle', 'carImages')
+      .populate("carId", "carTitle", "carImages")
       .exec();
 
     if (!userReviews || userReviews.length === 0) {
-      console.warn('No reviews found for this user.');
+      console.warn("No reviews found for this user.");
       return null;
     }
 
@@ -150,11 +143,24 @@ export async function fetchAllUsers(): Promise<UserParams[]> {
 
   const userDocuments = await User.find();
   if (userDocuments.length === 0) {
-    console.log('No user documents retrieved from the DB.');
+    console.log("No user documents retrieved from the DB.");
   } else {
     console.log(`Retrieved ${userDocuments.length} user(s) from the DB.`);
   }
 
   const usersArray = userDocuments.map((userDoc) => userDoc.toObject());
   return usersArray;
+}
+
+export async function getFavoriteCars(
+  userId: string
+): Promise<CarParamsExtended[]> {
+  await connectToDB();
+
+  const user = await User.findById(userId).populate("favoriteCars");
+  if (!user) {
+    throw new Error(`User with id ${userId} not found`);
+  }
+
+  return user.favoriteCars.map((car: any) => car.toObject());
 }
