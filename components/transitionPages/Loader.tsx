9a807f0e7dfe0c87d073a0f2a786e4cad68e109d
DevTaehong <minth1123@icloud.com>
2024-02-05 02:12:08 -0400

    refactor: Remove unused imports and reorganize imports in Loader component

--- components/transitionPages/Loader.tsx
+++ components/transitionPages/Loader.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React from "react";
 import Image from "next/image";
-import { loaderCityscape, advertWhiteCar } from "@/public/pngs";
 import { motion } from "framer-motion";
+
+import { loaderCityscape, advertWhiteCar } from "@/public/pngs";
 
 const Loader = () => {
   return (
