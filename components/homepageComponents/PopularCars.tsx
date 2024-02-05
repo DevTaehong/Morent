5315f8d4e69e314b07aa6ddf3546b72f8ef7deb2
DevTaehong <minth1123@icloud.com>
2024-02-05 01:51:46 -0400

    refactor: refactor PopularCars component and add userObjId prop

--- components/homepageComponents/PopularCars.tsx
+++ components/homepageComponents/PopularCars.tsx
@@ -1,37 +1,44 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import { addDays } from 'date-fns';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { addDays } from "date-fns";
+import { Types } from "mongoose";
 
-import CarCard from '../carCardComponents/CarCard';
-import { CarParamsExtended } from '@/lib/interfaces';
+import CarCard from "../carCardComponents/CarCard";
+import { CarParamsExtended } from "@/lib/interfaces";
 
 interface PopularCarsProps {
   popularCars: CarParamsExtended[] | any;
+  userObjId: Types.ObjectId | undefined;
 }
 
-const PopularCars: React.FC<PopularCarsProps> = ({ popularCars }) => {
+const PopularCars: React.FC<PopularCarsProps> = ({
+  popularCars,
+  userObjId,
+}) => {
+  const [viewAll, setViewAll] = useState(false);
   const twoDaysFromNow = addDays(new Date(), 2);
   const fiveDaysFromNow = addDays(new Date(), 5);
+
   popularCars = JSON.parse(popularCars);
-  const [viewAll, setViewAll] = useState(false);
+
   return (
     <motion.div
       animate={{ scale: 1 }}
       initial={{ scale: 0 }}
       className="flex w-full flex-col"
     >
-      <div className="mt-11 flex w-full justify-between px-5 xl:px-10">
+      <div className="mt-11 flex w-full justify-between px-6 xl:px-[4.94rem]">
         <p className="font-medium text-gray400">Popular Cars</p>
         <p
           className="cursor-pointer font-medium text-blue500"
           onClick={() => setViewAll((prev) => !prev)}
         >
-          {viewAll ? 'View Less' : 'View All'}
+          {viewAll ? "View Less" : "View All"}
         </p>
       </div>
-      <div className="no_scrollbar mt-4 flex w-full gap-5 overflow-x-auto xs:mt-0 xs:items-center xs:p-5 sm:grid sm:grid-cols-2 sm:flex-col sm:justify-center sm:gap-6 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="no_scrollbar mt-4 flex w-full gap-5 overflow-x-auto xs:mt-0 xs:items-center xs:p-6 sm:grid sm:grid-cols-2 sm:flex-col sm:justify-center sm:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:px-[3.75rem]">
         <div className="absolute right-0 h-60 w-20 bg-gradient-to-r from-transparent to-white/100 dark:to-gray900/100 xs:h-72 sm:hidden"></div>
         <div className="flex w-5 xs:hidden" />
 
@@ -43,6 +50,7 @@ const PopularCars: React.FC<PopularCarsProps> = ({ popularCars }) => {
                 isPopularCar={true}
                 availabilityFrom={twoDaysFromNow}
                 availabilityTo={fiveDaysFromNow}
+                hasLiked={car.likes?.some((like) => like === userObjId)}
               />
             ))
           : popularCars
@@ -54,6 +62,7 @@ const PopularCars: React.FC<PopularCarsProps> = ({ popularCars }) => {
                   isPopularCar={true}
                   availabilityFrom={twoDaysFromNow}
                   availabilityTo={fiveDaysFromNow}
+                  hasLiked={car.likes?.some((like) => like === userObjId)}
                 />
               ))}
       </div>
