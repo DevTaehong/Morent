b6135c79d9b7dfacd260aed437c1ee56c3f8dd2c
DevTaehong <minth1123@icloud.com>
2024-02-05 02:12:03 -0400

    refactor: Refactor search form component

--- components/searchFormComponents/SearchForm.tsx
+++ components/searchFormComponents/SearchForm.tsx
@@ -17,23 +17,21 @@ const SearchForm = () => {
     <div className="w-full">
       <div className="space-y-0 lg:mr-[0.81rem] lg:pt-[2.56rem]">
         <Label className="hidden dark:bg-blue100">Search</Label>
-        <>
-          <Label
-            htmlFor="search"
-            className="hidden pb-7 text-[0.75rem] font-semibold not-italic leading-[1.125rem] text-blue100 lg:block"
-          >
-            SEARCH
-          </Label>
-          <Input
-            onChange={handleInputChange}
-            id="search"
-            type="search"
-            className="h-12 truncate border-blue50 bg-[url('/svg-icons/filterSearch.svg')] bg-[0.75rem_center]  
+        <Label
+          htmlFor="search"
+          className="hidden pb-7 text-[0.75rem] font-semibold not-italic leading-[1.125rem] text-blue100 lg:block"
+        >
+          SEARCH
+        </Label>
+        <Input
+          onChange={handleInputChange}
+          id="search"
+          type="search"
+          className="h-12 truncate border-blue50 bg-[url('/svg-icons/filterSearch.svg')] bg-[0.75rem_center]  
               bg-no-repeat pl-12 text-[0.875rem] font-medium not-italic leading-[1.3125rem] tracking-[-0.0175rem] 
               text-gray700 dark:border-gray800 dark:bg-gray850 dark:bg-[url('/svg-icons/darkFilterSearch.svg')] dark:text-blue100"
-            placeholder="Search by car title"
-          />
-        </>
+          placeholder="Search by car title"
+        />
       </div>
     </div>
   );
