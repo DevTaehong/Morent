8008d9d0203b8d060f22390203445ee463bca387
DevTaehong <minth1123@icloud.com>
2024-02-05 01:51:53 -0400

    refactor: refactor RecommendedCars component

--- components/homepageComponents/RecommendedCars.tsx
+++ components/homepageComponents/RecommendedCars.tsx
@@ -1,18 +1,21 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Types } from "mongoose";
 
-import CarCard from '../carCardComponents/CarCard';
-import { addDays } from 'date-fns';
-import { CarParamsExtended } from '@/lib/interfaces';
+import CarCard from "../carCardComponents/CarCard";
+import { addDays } from "date-fns";
+import { CarParamsExtended } from "@/lib/interfaces";
 
 interface RecommendedCarsProps {
   recommendedCars: string;
+  userObjId: Types.ObjectId | undefined;
 }
 
 const RecommendedCars: React.FC<RecommendedCarsProps> = ({
   recommendedCars,
+  userObjId,
 }) => {
   const twoDaysFromNow = addDays(new Date(), 2);
   const fiveDaysFromNow = addDays(new Date(), 5);
@@ -26,10 +29,10 @@ const RecommendedCars: React.FC<RecommendedCarsProps> = ({
       initial={{ scale: 0 }}
       className="flex w-full flex-col"
     >
-      <p className="ml-5 mt-5 self-start font-medium text-gray400 xs:mt-0 xl:mx-10">
+      <p className="ml-6 mt-5 self-start font-medium text-gray400 xs:mt-0 xl:ml-[4.94rem]">
         Recommended cars
       </p>
-      <div className="mt-5 flex w-full flex-col items-center justify-center gap-5 px-5 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="mt-5 flex w-full flex-col items-center justify-center gap-5 px-6 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:px-[3.75rem]">
         {showAll
           ? parsedCars?.map((car: CarParamsExtended) => (
               <CarCard
@@ -37,6 +40,7 @@ const RecommendedCars: React.FC<RecommendedCarsProps> = ({
                 key={car._id}
                 availabilityFrom={twoDaysFromNow}
                 availabilityTo={fiveDaysFromNow}
+                hasLiked={car.likes?.some((like) => like === userObjId)}
               />
             ))
           : parsedCars
@@ -47,14 +51,15 @@ const RecommendedCars: React.FC<RecommendedCarsProps> = ({
                   key={car._id}
                   availabilityFrom={twoDaysFromNow}
                   availabilityTo={fiveDaysFromNow}
+                  hasLiked={car.likes?.some((like) => like === userObjId)}
                 />
               ))}
       </div>
       <button
-        className="my-10 max-w-[14.25rem] self-center rounded bg-blue500 px-10 py-4 text-sm font-medium text-white"
+        className="hover-effect my-10 max-w-[14.25rem] self-center rounded-[0.625rem] bg-blue500 px-10 py-4 text-sm font-medium text-white"
         onClick={() => setShowAll((prev) => !prev)}
       >
-        {showAll ? 'Show Less Cars' : 'Show More Cars'}
+        {showAll ? "Show Less Cars" : "Show More Cars"}
       </button>
     </motion.div>
   );
