e9f27fd4d5e05971ebb1370b67b6d395e453e7ba
DevTaehong <minth1123@icloud.com>
2024-02-05 02:46:59 -0400

    refactor: Refactor DialogPortal component and update close button styling

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -12,11 +12,8 @@ const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
-const DialogPortal = ({
-  className,
-  ...props
-}: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
+const DialogPortal = ({ ...props }: DialogPrimitive.DialogPortalProps) => (
+  <DialogPrimitive.Portal {...props} />
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
 
@@ -51,7 +48,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-6 top-8 rounded-[0.625rem] opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-white200 dark:data-[state=open]:text-slate-400">
-        <X className="h-4 w-4 dark:hidden" />
+        <X className="size-4 dark:hidden" />
         <Image src={darkClose} alt="close" width={16} height={16} />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
