40547e3639d7eb6f8b6b34f8d021d3e9bca3e0ae
DevTaehong <minth1123@icloud.com>
2024-02-05 02:13:24 -0400

    refactor: remove name validation from UserValidation

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -3,10 +3,6 @@ import * as z from "zod";
 export const UserValidation = z.object({
   image: z.string().url().nonempty(),
   id: z.string().nonempty(),
-  name: z
-    .string()
-    .min(3, { message: "Minimum 3 characters." })
-    .max(30, { message: "Maximum 30 characters." }),
   username: z
     .string()
     .min(3, { message: "Minimum 3 characters." })
