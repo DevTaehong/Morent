9f0a7f476d46848e113a4701b16c59da5a3f28d8
DevTaehong <minth1123@icloud.com>
2024-02-05 02:11:53 -0400

    refactor: Refactor UsersCarsForRent component

--- components/profilePageComponents/UsersCarsForRent.tsx
+++ components/profilePageComponents/UsersCarsForRent.tsx
@@ -1,18 +1,21 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Types } from "mongoose";
 
 import CarCard from "../carCardComponents/CarCard";
-import { CarParams } from "@/lib/interfaces";
 
 interface CarsForRentProps {
   carsForRent: string;
+  userObjId: Types.ObjectId | undefined;
 }
 
-const UsersCarsForRent: React.FC<CarsForRentProps> = ({ carsForRent }) => {
+const UsersCarsForRent: React.FC<CarsForRentProps> = ({
+  carsForRent,
+  userObjId,
+}) => {
   const [showMore, setShowMore] = useState(false);
-  const [parsedCarsForRent, setParsedCarsForRent] = useState<CarParams[]>([]);
+  const [parsedCarsForRent, setParsedCarsForRent] = useState<any[]>([]);
 
   useEffect(() => {
     if (carsForRent) {
@@ -24,42 +27,41 @@ const UsersCarsForRent: React.FC<CarsForRentProps> = ({ carsForRent }) => {
     <>
       <div className="flex w-full justify-between">
         <p className="mt-10 font-medium text-gray400">My Cars for Rent</p>
-        <p
-          className="mt-10 cursor-pointer font-medium text-gray400"
-          onClick={() => setShowMore((prev) => !prev)}
-        >
-          {showMore ? "See Less" : "See More"}
-        </p>
+        {parsedCarsForRent?.length > 4 && (
+          <button
+            className="mt-10 cursor-pointer font-medium text-gray400"
+            onClick={() => setShowMore((prev) => !prev)}
+          >
+            {showMore ? "See Less" : "See More"}
+          </button>
+        )}
       </div>
 
       <section className="mt-7 flex flex-col items-center gap-5 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {showMore && parsedCarsForRent?.length > 0
           ? parsedCarsForRent.map((car) => (
               // @ts-ignore
-              <CarCard carData={car} key={car._id} canEdit={true} />
+              <CarCard
+                carData={car}
+                key={car._id}
+                canEdit={true}
+                hasLiked={car.likes?.some(
+                  (like: Types.ObjectId) => like === userObjId
+                )}
+              />
             ))
           : parsedCarsForRent?.slice(0, 4).map((car) => (
               // @ts-ignore
-              <CarCard carData={car} key={car._id} canEdit={true} />
+              <CarCard
+                carData={car}
+                key={car._id}
+                canEdit={true}
+                hasLiked={car.likes?.some(
+                  (like: Types.ObjectId) => like === userObjId
+                )}
+              />
             ))}
-        <div>
-          {parsedCarsForRent?.length === 0 && (
-            <h3>You have not rented any cars. Yet.</h3>
-          )}
-        </div>
       </section>
-
-      {parsedCarsForRent?.length === 0 && (
-        <h3 className="p-5 font-medium text-gray400">
-          You have not added any cars to rent out.
-        </h3>
-      )}
-
-      <Link href="/cars/new">
-        <button className="mt-14 w-[14.25rem] self-center rounded-lg bg-blue500 p-5 font-semibold text-white">
-          Add More Cars for Rent
-        </button>
-      </Link>
     </>
   );
 };
