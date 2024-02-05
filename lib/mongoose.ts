bce4d83cc9cf916f23d8d8f16195a27da2c161bf
DevTaehong <minth1123@icloud.com>
2024-02-05 02:12:39 -0400

    refactor: refactor mongoose.ts for consistency and error handling

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,51 +1,43 @@
-import * as mongoose from 'mongoose';
-import { EventEmitter } from 'events';
+import * as mongoose from "mongoose";
+import { EventEmitter } from "events";
 
 export const dbEvents = new EventEmitter();
 
-let isConnected = false;
-
 const handleCarChange = (change: any) => {
   switch (change.operationType) {
-    case 'insert':
-      dbEvents.emit('carInserted', change.fullDocument);
+    case "insert":
+      dbEvents.emit("carInserted", change.fullDocument);
       break;
-    case 'update':
-      dbEvents.emit('carUpdated', change.fullDocument);
+    case "update":
+      dbEvents.emit("carUpdated", change.fullDocument);
       break;
-    case 'delete':
-      dbEvents.emit('carDeleted', change.documentKey._id);
+    case "delete":
+      dbEvents.emit("carDeleted", change.documentKey._id);
       break;
     default:
-      console.log('Unhandled operation type:', change.operationType);
+      console.log("Unhandled operation type:", change.operationType);
   }
 };
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) {
-    return console.log('Missing MongoDB URL');
-  }
-
-  if (isConnected) {
-    console.log('MongoDB connection already established');
-    return;
+    return console.log("Missing MongoDB URL");
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: 'TeamAlgoAlliance',
+      dbName: "TeamAlgoAlliance",
     });
-    isConnected = true;
 
-    const carChangeStream = mongoose.connection.collection('cars').watch();
+    const carChangeStream = mongoose.connection.collection("cars").watch();
 
-    carChangeStream.on('change', handleCarChange);
-    carChangeStream.on('error', (error: any) => {
-      console.error('Error with carChangeStream:', error);
+    carChangeStream.on("change", handleCarChange);
+    carChangeStream.on("error", (error: any) => {
+      console.error("Error with carChangeStream:", error);
     });
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
