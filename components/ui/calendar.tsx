d817836e63564fc73aff6d9cd5dce85c857ea83d
DevTaehong <minth1123@icloud.com>
2024-02-05 02:12:12 -0400

    refactor: Refactor UI styles in Calendar component

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -35,14 +35,14 @@ function Calendar({
         head_row: "flex",
         head_cell:
           "text-slate-500 rounded-md w-9 font-normal text-[0.8rem] dark:text-slate-400",
-        row: "flex w-full mt-2",
-        cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 dark:[&:has([aria-selected])]:bg-slate-800",
+        row: "flex w-full mt-2 rounded-md",
+        cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-[0.625rem] last:[&:has([aria-selected])]:rounded-r-[0.625rem] focus-within:relative focus-within:z-20 dark:[&:has([aria-selected])]:bg-slate-800",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         day_selected:
-          "bg-blue500 text-slate-50 hover:bg-slate-900 hover:text-slate-50 focus:bg-blue500 focus:text-slate-50 dark:bg-blue500 dark:text-slate-900 dark:hover:bg-slate-50 dark:hover:text-slate-900 dark:focus:bg-blue500 dark:focus:text-slate-900",
+          "bg-blue500 text-slate-50 hover:opacity-80 dark:hover:opacity-80 focus:bg-blue500 focus:text-slate-50 dark:bg-blue500 dark:text-slate-900 dark:focus:bg-blue500 dark:focus:text-slate-900",
         day_today:
           "bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50",
         day_outside: "text-slate-500 opacity-50 dark:text-slate-400",
@@ -53,8 +53,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="size-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="size-4" />,
       }}
       {...props}
     />
