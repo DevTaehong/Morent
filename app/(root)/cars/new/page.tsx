edc0260e03b78827675e74404ec642cea9063ef4
DevTaehong <minth1123@icloud.com>
2024-02-05 01:45:08 -0400

    refactor: refactor import statements in page.tsx

--- app/(root)/cars/new/page.tsx
+++ app/(root)/cars/new/page.tsx
@@ -1,8 +1,8 @@
-import { currentUser } from '@clerk/nextjs';
+import { currentUser } from "@clerk/nextjs";
 
-import CarForm from '@/components/forms/CarForm';
-import { userFromDB } from '@/lib/actions/user.actions';
-import { objectToStringId } from '@/utils/utility.serverFunctions';
+import CarForm from "@/components/forms/CarForm";
+import { userFromDB } from "@/lib/actions/user.actions";
+import { objectToStringId } from "@/utils/utility.serverFunctions";
 
 const Page = async () => {
   let user;
@@ -11,14 +11,14 @@ const Page = async () => {
 
   try {
     user = await currentUser();
-    userMongo = await userFromDB({ userName: user?.id });
+    userMongo = await userFromDB({ userClerkId: user?.id });
     userIdString = objectToStringId(userMongo?._id);
   } catch (err) {
     console.error(err);
   }
 
   return (
-    <div className="flex w-screen items-center justify-center bg-white200 dark:bg-gray900">
+    <div className="flex w-full items-center justify-center bg-white200 dark:bg-gray900">
       <div className="mb-12 mt-[7.6rem] flex w-screen max-w-4xl items-center justify-center px-5 md:mt-[9.4rem]">
         <CarForm userId={userIdString as string} />
       </div>
