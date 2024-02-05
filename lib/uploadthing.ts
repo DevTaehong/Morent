f70f913cf92f9a23b8367b998b58393e9b215184
DevTaehong <minth1123@icloud.com>
2024-02-05 02:12:50 -0400

    refactor: update import statements in uploadthing.ts

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,9 @@
-import { generateReactHelpers } from '@uploadthing/react/hooks';
+import { generateComponents } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
 
-import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
+
+export const { UploadButton } = generateComponents<OurFileRouter>();
