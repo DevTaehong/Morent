64ef8824f6e5735a7de777a54d438ce934bd4d43
DevTaehong <minth1123@icloud.com>
2024-02-10 16:49:52 -0400

    style: update padding top in Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ const Home = async () => {
 
   return (
     <main className="flex flex-col items-center bg-white200 dark:bg-gray900">
-      <div className="mt-24 flex w-full max-w-[90rem] flex-col items-center pt-5">
+      <div className="mt-24 flex w-full max-w-[90rem] flex-col items-center pt-8">
         <AdvertsDisplay />
         <div className="mt-7 flex w-full px-6 xl:px-[3.75rem]">
           <PickUpDropOffCard />
